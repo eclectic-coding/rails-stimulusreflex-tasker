fec52bee2be8003293dc527a195543beb291ee0d
Chuck Smith <chuck@eclecticsaddlebag.com>
2021-01-12 16:59:00 -0500

    Assign task to user

--- app/reflexes/list_reflex.rb
+++ app/reflexes/list_reflex.rb
@@ -1,9 +1,9 @@
 class ListReflex < StimulusReflex::Reflex
-  # delegate :current_user, to: :connection
+  delegate :current_user, to: :connection
 
   def create_task
     list = List.find(element.dataset.list_id)
-    @new_task = list.tasks.create(task_params.merge(creator: connection.current_user))
+    @new_task = list.tasks.create(task_params.merge(creator: current_user))
     @new_task = Task.new if @new_task.persisted?
   end
 
