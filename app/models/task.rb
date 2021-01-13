0e0c3d42890f25ce282192fd19bab1775b707a2d
Chuck Smith <chuck@eclecticsaddlebag.com>
2021-01-13 10:05:13 -0500

    Add completer to task UI and reflex

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
   acts_as_list scope: [:list_id, { completed_at: nil }], top_of_list: 0
   belongs_to :assignee, class_name: "User", optional: true
+  belongs_to :completer, class_name: "User", optional: true
   belongs_to :creator, class_name: "User"
   belongs_to :list
   validates :name, presence: true
@@ -8,6 +9,7 @@ class Task < ApplicationRecord
   scope :incomplete_first, -> { order(completed_at: :desc) }
 
   delegate :name, to: :assignee, prefix: true, allow_nil: true
+  delegate :name, to: :completer, prefix: true, allow_nil: true
 
   def complete?
     completed_at.present?
